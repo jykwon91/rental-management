81ab2388b17e4e9e07526f29dd4d3fee37f1eecc
Jason Kwon <jasonykwon91@gmail.com>
2020-10-08 10:03:14 -0500

    Upvote/downvote, update cache

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,13 +1,15 @@
-import { Field, ObjectType } from 'type-graphql';
+import { Field, Int, ObjectType } from 'type-graphql';
 import {
   BaseEntity,
   Column,
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Updoot } from './Updoot';
 import { User } from './User';
 
 @ObjectType()
@@ -29,10 +31,17 @@ export class Post extends BaseEntity {
   @Column({ type: 'int', default: 0 })
   points!: number;
 
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null; // 1 or -1 or null
+
   @Field()
   @Column()
   creatorId: number;
 
+  @OneToMany(() => Updoot, (updoot) => updoot.post)
+  updoots: Updoot[];
+
+  @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
 
---
+++ web/src/components/UpdootSection.tsx
@@ -0,0 +1,62 @@
+import { Flex, Box, IconButton, usePopoverContext } from '@chakra-ui/core';
+import React, { useState } from 'react';
+import {
+  PostSnippetFragment,
+  PostsQuery,
+  useVoteMutation,
+} from '../generated/graphql';
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    'updoot-loading' | 'downdoot-loading' | 'not-loading'
+  >('not-loading');
+
+  const [, vote] = useVoteMutation();
+  return (
+    <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
+      <Box>
+        <IconButton
+          onClick={async () => {
+            if (post.voteStatus === 1) {
+              return;
+            }
+            setLoadingState('updoot-loading');
+            await vote({
+              postId: post.id,
+              value: 1,
+            });
+            setLoadingState('not-loading');
+          }}
+          variantColor={post.voteStatus === 1 ? 'green' : undefined}
+          isLoading={loadingState === 'updoot-loading'}
+          aria-label="updoot post"
+          icon="chevron-up"
+        />
+      </Box>
+      <Box>{post.points}</Box>
+      <Box>
+        <IconButton
+          onClick={async () => {
+            if (post.voteStatus === -1) {
+              return;
+            }
+            setLoadingState('downdoot-loading');
+            await vote({
+              postId: post.id,
+              value: -1,
+            });
+            setLoadingState('not-loading');
+          }}
+          variantColor={post.voteStatus === -1 ? 'red' : undefined}
+          isLoading={loadingState === 'downdoot-loading'}
+          aria-label="downvote post"
+          icon="chevron-down"
+        />
+      </Box>
+    </Flex>
+  );
+};
