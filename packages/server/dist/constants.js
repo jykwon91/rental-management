d358913056fd16336a93104d9c197bb57587dfbe
Jason Kwon <jasonykwon91@gmail.com>
2020-10-14 14:30:43 -0500

    Changed directory structure, added graphql support for mobile app

---
+++ packages/server/dist/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FORGET_PASSWORD_PREFIX = exports.__prod__ = exports.COOKIE_NAME = void 0;
+exports.COOKIE_NAME = 'qid';
+exports.__prod__ = process.env.NODE_ENV === 'production';
+exports.FORGET_PASSWORD_PREFIX = 'forget-password:';
+//# sourceMappingURL=constants.js.map
